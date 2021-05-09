8e35b695b4f47437955c5be53326a6e82fa41310
tharukshi724 <tharukshiw@gmail.com>
2021-05-09 11:23:02 +0530

    add myadapter file

---
+++ app/src/main/java/com/example/chopchop_recipeapp/MyAdapter.java
@@ -0,0 +1,198 @@
+package com.example.chopchop_recipeapp;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+
+    List<Pantry> modelList;
+
+
+
+    public MyAdapter(List<Pantry> modelList) {
+        this.modelList = modelList;
+
+
+
+
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row,parent,false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Pantry pantry = this.modelList.get(position);
+
+        holder.item.setText(pantry.getItem());
+        holder.size.setText(pantry.getSize());
+
+
+        try{
+            String d1= this.modelList.get(position).getDate();
+            SimpleDateFormat dates = new SimpleDateFormat("yyyy/MM/dd");
+            String currentDate = dates.format(new Date());
+            Date date1;
+            Date date2;
+            Long b1 = Long.valueOf(14);
+            Long b2 = Long.valueOf(30);
+            date1 = dates.parse(d1);
+            date2 = dates.parse(currentDate);
+
+
+            long  difference =  Math.abs(date1.getTime() - date2.getTime());
+
+
+            long differenceDates = difference / (24 * 60 * 60 * 1000);
+
+            String dayDifference = Long.toString(differenceDates);
+            pantry.setDayDifference(dayDifference);
+
+
+
+
+
+
+
+
+        } catch (Exception exception) {
+
+        }
+        holder.dayDifference.setText(pantry.getDayDifference());
+
+
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DialogPlus dia = DialogPlus.newDialog(holder.edit.getContext())
+                        .setContentHolder(new ViewHolder(R.layout.dialogcontent))
+                        .setExpanded(true,600)
+                        .create();
+
+                View myview = dia.getHolderView();
+
+                EditText item = myview.findViewById(R.id.item);
+                EditText size = myview.findViewById(R.id.size);
+                EditText date = myview.findViewById(R.id.date);
+                Button btn = myview.findViewById(R.id.updateBtn);
+
+                item.setText(pantry.getItem());
+                size.setText(pantry.getSize());
+                date.setText(pantry.getDate());
+
+                dia.show();
+
+            //   btn.setOnClickListener(new View.OnClickListener() {
+                //   @Override
+                 //  public void onClick(View v) {
+                 //      Map<String,Object> map = new HashMap<>();
+                  //     map.put("item",item.getText().toString());
+                   //    map.put("size",size.getText().toString());
+                   //    map.put("date",date.getText().toString());
+
+                      //  FirebaseDatabase.getInstance().getReference().child("Pantry")
+                           //    .child(getRef(position).getKey()).updateChildren(map)
+                           //    .addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                          //         public void onSuccess(Void aVoid) {
+                                 //      dia.dismiss();
+                               //    }
+                        //       })
+                     //  .addOnFailureListener(new OnFailureListener() {
+
+                     //      public void onFailure(@NonNull Exception e) {
+                           //    dia.dismiss();
+                         //  }
+                    //   });
+
+                //   }
+             //  });
+
+
+            }
+        });
+
+
+    }
+
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return modelList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+         TextView item;
+        TextView size;
+        TextView dayDifference;
+
+        Button edit;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            item = (TextView)itemView.findViewById(R.id.text1);
+            size = (TextView)itemView.findViewById(R.id.text3);
+            dayDifference = (TextView)itemView.findViewById(R.id.text2);
+            edit = itemView.findViewById(R.id.edit);
+        }
+    }
+
+    public void deleteItem(int position){
+      modelList.remove(position);
+       // String key = keys.get(position);
+       // DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Model");
+        //ref.child(key).removeValue();
+        //getSnapshots().getSnapshot(position).getReference().delete();
+      notifyItemRemoved(position);
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
