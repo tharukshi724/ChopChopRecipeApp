593477a4a6331205475bfb9b690941b0f469499a
iamKAviB <iamkavib@gmail.com>
2021-05-09 12:54:28 +0530

    gerocery(extra )

---
+++ app/src/main/java/com/example/chopchop_recipeapp/Test.java
@@ -0,0 +1,54 @@
+package com.example.chopchop_recipeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Test extends AppCompatActivity {
+
+    EditText input;
+    Button save;
+    DatabaseReference dbRef;
+    int i=0;
+    String test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        input=findViewById(R.id.input);
+        save=findViewById(R.id.buttoninput);
+
+        TestJava obj = new TestJava();
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbRef = FirebaseDatabase.getInstance().getReference().child("test");
+                try{
+                    if(TextUtils.isEmpty(input.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"pls enter the input",Toast.LENGTH_LONG).show();
+                    else{
+                        obj.setIn(input.getText().toString().trim());
+                        i++;
+                        test = String.valueOf(i);
+                        dbRef.child(test).setValue(obj);
+                        Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(),"error",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
